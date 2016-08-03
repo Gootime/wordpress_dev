000996bb60ae4e1ee0536d9e2547676d0bf68382
Gootime <gotimeingame@gmail.com>
2016-08-03 14:47:34 +0200

    change

--- category.php
+++ category.php
@@ -4,39 +4,30 @@
 */
 
 get_header(); ?> 
-
-<section id="primary" class="site-content">
-<div id="content" role="main">
-
 <?php
-  	$cat = get_query_var('cat');
-  	$yourcat = get_category ($cat);
-query_posts('category_name='.get_the_title().'&showpost=12');
-// Check if there are any posts to display
-if ( have_posts() ) { 
 ?>
-<header class="archive-header">
-<h1 class="archive-title">Category: <?php echo $yourcat->name; ?></h1>
+<header class="header">
+<h1 class="entry-title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></h1>
 </header>
-
 <?php
+	$cat = get_query_var('cat');
+  	$yourcat = get_category ($cat);
 
-// The Loop
-while ( have_posts() ) : the_post(); ?>
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-<?php endwhile; 
+$cats = array();
+foreach (get_the_category($post_id) as $c) {
+$cat = get_category($c);
+array_push($cats, $cat->name);
 }
 
-else{ ?>
-<p>Sorry, no posts matched your criteria.</p>
-<?php
+if (sizeOf($cats) > 0) {
+$post_categories = implode(', ', $cats);
+} else {
+$post_categories = 'Not Assigned';
 }
+
+echo $post_categories;
 ?>
-
-</div>
+	</div>
 </section>
 
-<?php wp_reset_query(); ?>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
